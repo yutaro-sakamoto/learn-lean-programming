cdf64b78dd0aac65287b69f3e77f54f9d31a3e2b
yutaro-sakamoto <80912876+yutaro-sakamoto@users.noreply.github.com>
2025-10-22 18:40:41 +0900

    Correct main function and example output
    Fix syntax issues and improve output formatting.

--- Main.lean
+++ Main.lean
@@ -2,7 +2,10 @@ import LearnLeanProgramming
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
+  retu
 
 #eval s!"The answer is {2 + 2}"
 
 example := Nat.succ 2
+
+#eval (pure "hello" >>= IO.println)
